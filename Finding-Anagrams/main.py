c6eb05a1e8c26fa7e570879e7690c9f9c322ea87
OluwakemiVandy <kemivandy@gmail.com>
2022-05-28 15:28:38 +0100

    first commit

---
+++ Finding-Anagrams/main.py
@@ -0,0 +1,26 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+from pickle import FALSE
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    str1 = input("Please input word: ")
+    str2 = input("Please input anagram: ")
+
+    str1 = str1.lower()
+    str2 = str2.lower()
+    if(len(str1) == len(str2)):
+
+     sorted_str1 = sorted(str1)
+     sorted_str2 = sorted(str2)
+
+    if(sorted_str1== sorted_str2):
+        return True
+    else:
+        return False
+    #return True
+
+print(find_anagram(str, str))
